ee2823e630edaa121c5182f04f2f82707e931cbf
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-01-13 16:18:42 +0700

    feat: fix .env

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,8 +1,8 @@
-// export const KEY_COOKIE = 'token';
-// export const API_URL =
-//   'https://express-auth-boilerplate-production.up.railway.app';
+export const KEY_COOKIE = 'token';
+export const API_URL =
+  'https://express-auth-boilerplate-production.up.railway.app';
 
 // export const API_URL = 'http://localhost:5000';
 
-export const KEY_COOKIE = process.env.NEXT_PUBLIC_COOKIE_KEY_NAME || '';
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || '';
+// export const KEY_COOKIE = process.env.NEXT_PUBLIC_COOKIE_KEY_NAME ;
+// export const API_URL = process.env.NEXT_PUBLIC_API_URL ;
